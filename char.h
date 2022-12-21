3e4cc7edab688864f0caf2878dca5a077309e405
Flanky <flankedgonerogue@gmail.com>
2022-12-21 09:09:09 +0500

    + length() + addEql() + tested both, working correctly

---
+++ char.h
@@ -0,0 +1,37 @@
+//
+// Created by Flanky on 12/21/2022.
+//
+
+#ifndef CS101_PROJECT_CHAR_H
+#define CS101_PROJECT_CHAR_H
+
+namespace flanky
+{
+    int length(char* a)
+    {
+        int length = 0;
+        while (*a != '\0')
+        {
+            length++;
+            a++;
+        }
+        return length;
+    }
+
+    void addEql(char* a, char* b)
+    {
+        while (*a != '\0')
+        {
+            a++;
+        }
+        while (*b != '\0')
+        {
+            *a = *b;
+            a++;
+            b++;
+        }
+        *a = '\0';
+    }
+}
+
+#endif //CS101_PROJECT_CHAR_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "char.h"
+
+using namespace std;
+namespace f = flanky;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
+    char text[] = "Hello";
+    char text1[] = "World!";
+    cout << f::length(text);
+    f::addEql(text, text1);
+    cout << text;
     return 0;
 }
