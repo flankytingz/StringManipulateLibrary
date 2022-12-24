1112d24c25b4221978dac084375f6fb03a40a932
Flanky <flankedgonerogue@gmail.com>
2022-12-24 17:22:11 +0500

    tested all functions in namespace flanky

--- main.cpp
+++ main.cpp
@@ -3,21 +3,49 @@
 
 using namespace std;
 namespace f = flanky;
+char* str;
+char* str1 = static_cast<char *>(malloc(sizeof(char) * 6));
+char* str2 = static_cast<char *>(malloc(sizeof(char) * 6));
+
+void printStrings()
+{
+    cout << "str1: " << str1 << endl;
+    cout << "str2: " << str2 << endl;
+    cout << "str: " << str << endl;
+}
 
 int main() {
-    cout << "Hello, World!" << std::endl;
-    char text[] = "Hell";
-    char text1[] = "Hello";
-    char* str = f::shiftRight(text, 2);
+    memcpy(str1, "Hello", sizeof("Hello"));
+    memcpy(str2, "World", sizeof("World"));
+    str = f::add(str1, str2);
+    cout << "str: " << str << endl; // outputs str: HelloWorld
 
-    while (*str != '\0')
-    {
-        cout << *str << endl;
-        str++;
-    }
+    f::addEql(str1, str2);
+    cout << "str1: " << str1 << endl; // outputs str1: HelloWorld
 
-    cout << endl;
+    char c = f::getChar(str1, 1);
+    cout << "char: " << c << endl; // outputs char: e
 
-    for (const char& letter : text) cout << letter;
+    f::setChar(str1, 0, 'A');
+    cout << "str1: " << str1 << endl; // outputs str1: AelloWorld
+
+    cout << f::isEqual(str1, str2) << endl; // outputs 0
+    cout << f::isGreater(str1, str2) << endl; // outputs 1
+    cout << f::isSmaller(str1, str2) << endl; // outputs 0
+
+    free(str);
+    str = f::getSubstring(str1, 2, 5);
+    cout << "str: " << str << endl; // outputs str: lloWo
+
+    free(str);
+    str = f::shiftLeft(str1, 5);
+    cout << "str: " << str << endl; // outputs str: Aello
+    cout << "str1: " << str1 << endl; // outputs str1: World
+
+    free(str);
+    str = f::shiftRight(str1, 3);
+    cout << "str: " << str << endl; // outputs str: dlr
+    cout << "str1: " << str1 << endl; // outputs str1: Wo
+
     return 0;
 }
