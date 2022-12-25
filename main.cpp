89d28c02861e17b90b26eb29ec62e96ea7d2cf71
Flanky <flankedgonerogue@gmail.com>
2022-12-25 15:55:23 +0500

    using char[] objects instead of malloc

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 namespace f = flanky;
 char* str;
-char* str1 = static_cast<char *>(malloc(sizeof(char) * 6));
-char* str2 = static_cast<char *>(malloc(sizeof(char) * 6));
-
+char* str1 = new char[6];
+char* str2 = new char[6];
 
 int main() {
     memcpy(str1, "Hello", sizeof("Hello"));
     memcpy(str2, "World", sizeof("World"));
+
     str = f::add(str1, str2);
     cout << "str: " << str << endl; // outputs str: HelloWorld
 
@@ -27,16 +27,17 @@ int main() {
     cout << f::isGreater(str1, str2) << endl; // outputs 1
     cout << f::isSmaller(str1, str2) << endl; // outputs 0
 
-    free(str);
+    delete str;
     str = f::getSubstring(str1, 2, 5);
     cout << "str: " << str << endl; // outputs str: lloWo
 
-    free(str);
+    delete str;
+
     str = f::shiftLeft(str1, 5);
     cout << "str: " << str << endl; // outputs str: Aello
     cout << "str1: " << str1 << endl; // outputs str1: World
 
-    free(str);
+    delete str;
     str = f::shiftRight(str1, 3);
     cout << "str: " << str << endl; // outputs str: dlr
     cout << "str1: " << str1 << endl; // outputs str1: Wo
